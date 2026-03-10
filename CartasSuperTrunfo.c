5624253b95c706ff30ad106839c2a9947909094e
glucasrss <ncaygl10@gmail.com>
2026-03-10 11:05:30 +0000

    Desafio Nível Aventureiro Adicionado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,6 +16,9 @@ int main() {
     char EstadoA, EstadoB;
     char CódigoA[5], CódigoB[5];
     char NomeA [20], NomeB [20];
+
+    float Densidade_PopulacionalA, Densidade_PopulacionalB;
+    float PIB_per_CapitaA, PIB_per_CapitaB;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -47,6 +50,9 @@ int main() {
 
     printf("\nA primeira carta foi configurada com sucesso!\n");
 
+    Densidade_PopulacionalA = (float) PopulaçãoA/ÁreaA;
+    PIB_per_CapitaA = (float) PIBA/PopulaçãoA;
+
     //Cadastro da Segunda Carta
     printf("\nAgora vamos configurar a segunda carta, digite os valores solicitados a seguir!\n");
 
@@ -74,6 +80,9 @@ int main() {
 
     printf("\nTudo pronto! Agora confira os valores das cartas!\n");
 
+    Densidade_PopulacionalB = (float) PopulaçãoB/ÁreaB;
+    PIB_per_CapitaB = (float) PIBB/PopulaçãoB;
+
     // Exibição dos Dados das Cartas:
 
     //Exibição da Primeira Carta
@@ -83,20 +92,24 @@ int main() {
     printf("\nCódigo da Carta: %s", CódigoA);
     printf("\nNome da Cidade: %s", NomeA);
     printf("\nPopulação: %d", PopulaçãoA);
-    printf("\nÁrea (em km²): %f", ÁreaA);
-    printf("\nPIB: %f", PIBA);
-    printf("\nNúmero de Pontos Turísticos: %d\n", Pontos_TurísticosA);
+    printf("\nÁrea (em km²): %.2f", ÁreaA);
+    printf("\nPIB: %.2f", PIBA);
+    printf("\nNúmero de Pontos Turísticos: %d", Pontos_TurísticosA);
+    printf("\nDensidade Populacional: %.2f", Densidade_PopulacionalA);
+    printf("\nPIB per Capita: %.2f\n", PIB_per_CapitaA);
 
     //Exibição da Segunda Carta
-     printf("\nSEGUNDA CARTA");
+    printf("\nSEGUNDA CARTA");
 
     printf("\nEstado: %c", EstadoB);
     printf("\nCódigo da Carta: %s", CódigoB);
     printf("\nNome da Cidade: %s", NomeB);
     printf("\nPopulação: %d", PopulaçãoB);
-    printf("\nÁrea (em km²): %f", ÁreaB);
-    printf("\nPIB: %f", PIBB);
+    printf("\nÁrea (em km²): %.2f", ÁreaB);
+    printf("\nPIB: %.2f", PIBB);
     printf("\nNúmero de Pontos Turísticos: %d", Pontos_TurísticosB);
+    printf("\nDensidade Populacional: %.2f", Densidade_PopulacionalB);
+    printf("\nPIB per Capita: %.2f", PIB_per_CapitaB);
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
